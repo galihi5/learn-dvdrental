3e9b50f9fe69d910bcc75d0dab951ad1d1b865c6
galihi5 <www.galih@gmail.com>
2022-08-04 14:10:36 +0700

    initial commit

---
+++ src/main/java/com/gaw/learndvdrental/LearnDvdrentalApplication.java
@@ -0,0 +1,13 @@
+package com.gaw.learndvdrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnDvdrentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnDvdrentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaw/learndvdrental/LearnDvdrentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaw.learndvdrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnDvdrentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
